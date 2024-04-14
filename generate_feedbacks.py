cda75439ebf910eac2ef50357d16cbfedc8590ce
IlyesPyDE <153717308+IlyesPyDE@users.noreply.github.com>
2024-04-14 13:21:00 +0200

    modification du generate_feedbacks.py

--- generate_feedbacks.py
+++ generate_feedbacks.py
@@ -5,7 +5,16 @@ from datetime import datetime, timedelta
 # Définition des données possible pour chaque champ
 bootcamps = ['data_engineering', 'data_science', 'web_development', 'cyber_security', 'artificial_intelligence', 'cloud_computing']
 feedback_types = ['contenu_cours', 'formateur', 'methodes_pedagogiques', 'support_cours', 'environnement_apprentissage', 'communication', 'axe_amelioration']
-comments = ['satisfaisant', 'à améliorer', 'tres moyen', 'moyen', 'pas mal', 'pourrait être mieux']
+
+# Commentaires liés aux plages de notes
+ratings_comments = {
+    (0, 1): "à fuir",
+    (2, 3): "plutôt mauvais",
+    (4, 5): "moyen",
+    (6, 7): "pas mal",
+    (8, 9): "très bon",
+    (10, 10): "excellent"
+}
 
 # Fonction pour générer une date aléatoire entre deux bornes
 def random_date(start, end):
@@ -25,7 +34,7 @@ with open('feedbacks.csv', 'w', newline='', encoding='utf-8') as file:
         feedback_type = random.choice(feedback_types)
         date = random_date(start_date, end_date)
         rating = random.randint(0, 10)  # Génération d'une note aléatoire entre 0 et 10
-        comment = random.choice(comments)
+        comment = next(comment for range_, comment in ratings_comments.items() if range_[0] <= rating <= range_[1])
         
         writer.writerow([bootcamp, feedback_type, date, rating, comment])
 
