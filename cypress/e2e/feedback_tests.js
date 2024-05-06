8f4c617d9b7d82656a921aea2502a1374a63a14f
Ilyes <salmi.ilyes@gmail.com>
2024-05-06 14:25:08 +0200

    update et ajout de test Cypress

--- cypress/e2e/feedback_tests.js
+++ cypress/e2e/feedback_tests.js
@@ -1,16 +1,1 @@
-describe('Feedback Tests', () => {
-    beforeEach(() => {
-        // Visitez la page d'accueil avant chaque test
-        cy.visit('http://localhost:5000');
-    });
-
-    it('should submit a feedback', () => {
-        //Remplissez le formulaire de feedback
-        cy.get('select[name="formation"]').select('data_engineering');
-        cy.get('select[name="typeRetour"]').select('contenu_cours');
-        cy.get('input["date"]').type('2023-05-29');
-        cy.get('input[name="rating"]').type('4');
-        cy.get('textarea[name="comments"]').type('cours interessant');
-        ...
-    }
-)
+describe
